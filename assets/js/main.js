b9da2ecfa5d9c46f4b823f2063e9388d5237f5b1
emilycsm26 <emilysamuels26@gmail.com>
2024-09-05 22:23:31 -0400

    Changing template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	TXT by HTML5 UP
+	Telephasic by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -7,16 +7,15 @@
 (function($) {
 
 	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+		$body = $('body');
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ '361px',   '736px'  ],
-			xsmall:  [ null,      '360px'  ]
+			normal:    [ '1081px',  '1280px'  ],
+			narrow:    [ '821px',   '1080px'  ],
+			narrower:  [ '737px',   '820px'   ],
+			mobile:    [ '481px',   '736px'   ],
+			mobilep:   [ null,      '480px'   ]
 		});
 
 	// Play initial animations on page load.
@@ -29,24 +28,17 @@
 	// Dropdowns.
 		$('#nav > ul').dropotron({
 			mode: 'fade',
-			noOpenerFade: true,
 			speed: 300,
-			alignment: 'center'
-		});
-
-	// Scrolly
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height() - 5; }
+			alignment: 'center',
+			noOpenerFade: true
 		});
 
 	// Nav.
 
-		// Title Bar.
+		// Button.
 			$(
-				'<div id="titleBar">' +
+				'<div id="navButton">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
@@ -55,6 +47,7 @@
 			$(
 				'<div id="navPanel">' +
 					'<nav>' +
+						'<a href="index.html" class="link depth-0">Home</a>' +
 						$('#nav').navList() +
 					'</nav>' +
 				'</div>'
@@ -63,10 +56,9 @@
 				.panel({
 					delay: 500,
 					hideOnClick: true,
-					hideOnSwipe: true,
 					resetScroll: true,
 					resetForms: true,
-					side: 'left',
+					side: 'top',
 					target: $body,
 					visibleClass: 'navPanel-visible'
 				});
